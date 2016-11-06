e85227aded4be8aa67a3fdeaa232070f4bc0af14
sxend <arimitsu.k+github@gmail.com>
2016-11-06 23:48:18 +0900

    simplify

--- src/main/scala/arimitsu/sf/platform/interceptor/LoggingInterceptor.scala
+++ src/main/scala/arimitsu/sf/platform/interceptor/LoggingInterceptor.scala
@@ -1,23 +1,11 @@
 package arimitsu.sf.platform.interceptor
 
-import akka.event.LoggingAdapter
-import akka.http.scaladsl.model.HttpRequest
-import akka.http.scaladsl.server.{ Route, RouteResult }
-import akka.http.scaladsl.server.directives.{ DebuggingDirectives, LoggingMagnet }
-import scala.collection.JavaConverters._
+import akka.event.Logging
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
 
 class LoggingInterceptor(env: {
 }) {
-  def intercept(underlying: => Route) = logging(underlying)
-  private def outputLog(logger: LoggingAdapter) = (req: HttpRequest) => (res: RouteResult) => {
-    res match {
-      case complete: RouteResult.Complete =>
-        logger.info(s"Completed: Request: ${req.method.value} ${req.uri.toString} ${complete.getResponse.status}")
-      case rejected: RouteResult.Rejected =>
-        logger.info(s"Rejected: Request: ${req.method.value} ${req.uri.toString} ${rejected.getRejections.asScala.toList}")
-      case _ =>
-    }
-  }
-  private val logging =
-    DebuggingDirectives.logRequestResult(LoggingMagnet(logger => outputLog(logger)))
+  def intercept(underlying: => Route) =
+    logRequestResult("access-log", Logging.InfoLevel)(underlying)
 }
