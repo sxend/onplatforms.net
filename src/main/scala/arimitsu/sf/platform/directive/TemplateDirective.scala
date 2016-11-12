5b38098313109ad4adef5ba2b92a8ca1b4b33399
sxend <arimitsu.k+github@gmail.com>
2016-11-12 17:46:59 +0900

    refactor

--- src/main/scala/arimitsu/sf/platform/directive/TemplateDirective.scala
+++ src/main/scala/arimitsu/sf/platform/directive/TemplateDirective.scala
@@ -1,15 +1,15 @@
 package arimitsu.sf.platform.directive
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.server.directives.BasicDirectives.{extractSettings => _, pass => _}
-import akka.http.scaladsl.server.directives.CacheConditionDirectives.{conditional => _}
-import akka.http.scaladsl.server.directives.RouteDirectives.{complete => _, reject => _}
+import akka.http.scaladsl.server.directives.BasicDirectives.{ extractSettings => _, pass => _ }
+import akka.http.scaladsl.server.directives.CacheConditionDirectives.{ conditional => _ }
+import akka.http.scaladsl.server.directives.RouteDirectives.{ complete => _, reject => _ }
 import com.typesafe.config.ConfigFactory
 import org.fusesource.scalate.TemplateEngine
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.Try
 
 trait TemplateDirective {
@@ -21,8 +21,7 @@ trait TemplateDirective {
     _engine
   }
 
-  def template(resourceName: String, attributes: Map[String, Any])(f: Try[String] => Route)
-              (implicit implicits: TemplateDirective.Implicits) =
+  def template(resourceName: String, attributes: Map[String, Any])(f: Try[String] => Route)(implicit implicits: TemplateDirective.Implicits) =
     onComplete(Future(engine.layout(resourceName, attributes))(implicits.ioDispatcher))(f)
 
 }
--- src/main/scala/arimitsu/sf/platform/router/IndexRouter.scala
+++ src/main/scala/arimitsu/sf/platform/router/IndexRouter.scala
@@ -3,6 +3,7 @@ package arimitsu.sf.platform.router
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
+
 import arimitsu.sf.platform.Directives._
 import arimitsu.sf.platform.directive.TemplateDirective
 
@@ -14,9 +15,9 @@ class IndexRouter(env: {
   implicit val templateImplicit = TemplateDirective.Implicits(env.system)
 
   def handle = template("templates/index.html", Map("value" -> "foo")) {
-    case Success(html) =>
-      complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, html))
-    case _ => reject
+    case Success(html) => complete(htmlEntity(html))
+    case _             => reject
   }
-
+  private def htmlEntity(html: String) =
+    HttpEntity(ContentTypes.`text/html(UTF-8)`, html)
 }
