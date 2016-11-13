8270d1088973e93f5ae7d6d0d3c722c7a1f70bb9
sxend <arimitsu.k+github@gmail.com>
2016-11-13 23:44:21 +0900

    refactor

--- src/main/scala/arimitsu/sf/platform/router/SigninRouter.scala
+++ src/main/scala/arimitsu/sf/platform/router/SigninRouter.scala
@@ -3,19 +3,13 @@ package arimitsu.sf.platform.router
 import java.util.UUID
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpHeader.ParsingResult
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.directives.Credentials
 import arimitsu.sf.platform.Directives._
 import arimitsu.sf.platform.directive.{ AuthenticationDirective, TemplateDirective }
 import arimitsu.sf.platform.external.Twitter
 import arimitsu.sf.platform.kvs.Memcached
-import com.typesafe.config.ConfigFactory
 
-import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.{ Failure, Success }
 
@@ -34,7 +28,6 @@ class SigninRouter(env: {
 
   import env._
   import system.dispatcher
-  private val logger = system.log
 
   def handle = template("templates/signin.html") {
     case Success(html) => complete(htmlEntity(html))
@@ -46,38 +39,13 @@ class SigninRouter(env: {
     case _            => reject
   }
 
-  def twitterCallback = parameters("oauth_token", "oauth_verifier") { (oauthToken, oauthVerifier) =>
-    onComplete(env.twitter.verify(oauthVerifier)) {
-      case Success((twitterUserId, twitterUserName)) =>
-        println(s"twitterUserId: $twitterUserId, twitterUserName: $twitterUserName")
-        onComplete(env.twitter.getMappedUser(twitterUserId = twitterUserId)) {
-          case Success(Some(userId)) =>
-            setSession(UUID.randomUUID().toString, userId)(redirect("/mypage", StatusCodes.Found))
-          case Success(None) =>
-            val userId = UUID.randomUUID().toString
-            onComplete(env.twitter.mappingUser(twitterUserId, userId)) {
-              case Success(_) =>
-                onComplete(registerUser(userId, twitterUserName)) {
-                  case Success(_) =>
-                    setSession(UUID.randomUUID().toString, userId)(redirect("/mypage", StatusCodes.Found))
-                  case Failure(t) =>
-                    logger.error(t, t.getMessage)
-                    top
-                }
-              case Failure(t) =>
-                logger.error(t, t.getMessage)
-                top
-            }
-          case Failure(t) =>
-            logger.error(t, t.getMessage)
-            top
-        }
-      case Failure(t) =>
-        logger.error(t, t.getMessage)
-        top
-    }
+  def twitterCallback = parameters("oauth_token", "oauth_verifier") { (_, oauthVerifier) =>
+    TwitterSignin.callback(oauthVerifier)
   }
-  private def top = redirect("/", StatusCodes.Found)
+
+  private def genUUID = UUID.randomUUID().toString
+  private def succeed(userId: String) =
+    setSession(genUUID, userId)(redirect("/mypage", StatusCodes.Found))
 
   private def registerUser(userId: String, name: String) = {
     env.memcached.client.set(userId, name, Int.MaxValue.seconds).map { x =>
@@ -85,17 +53,36 @@ class SigninRouter(env: {
     }
   }
 
-  private def authenticator(credentials: Credentials): Future[Option[String]] = {
-    import system.dispatcher
-    credentials match {
-      case p @ Credentials.Provided(token) =>
-        Future(Option(token))
-      case _ => Future.successful(None)
-    }
-  }
-
   private def htmlEntity(html: String) =
     HttpEntity(ContentTypes.`text/html(UTF-8)`, html)
+  object TwitterSignin {
+    def callback(oauthVerifier: String) =
+      onComplete(env.twitter.verify(oauthVerifier)) {
+        case Success((twitterUserId, twitterUserName)) =>
+          TwitterSignin.verify(twitterUserId, twitterUserName)
+        case Failure(t) => failWith(t)
+      }
+    private def verify(twitterUserId: String, twitterUserName: String) =
+      onComplete(env.twitter.getMappedUser(twitterUserId = twitterUserId)) {
+        case Success(Some(userId)) =>
+          succeed(userId)
+        case Success(None) =>
+          mappingAndRegister(genUUID, twitterUserId, twitterUserName)
+        case Failure(t) => failWith(t)
+      }
+    private def mappingAndRegister(userId: String, twitterUserId: String, twitterUserName: String) =
+      onComplete(env.twitter.mappingUser(twitterUserId, userId)) {
+        case Success(_) =>
+          registerTwitterUser(userId, twitterUserName)
+        case Failure(t) => failWith(t)
+      }
+    private def registerTwitterUser(userId: String, twitterUserName: String) =
+      onComplete(registerUser(userId, twitterUserName)) {
+        case Success(_) =>
+          succeed(userId)
+        case Failure(t) => failWith(t)
+      }
+  }
 }
 
 object SigninRouter {
