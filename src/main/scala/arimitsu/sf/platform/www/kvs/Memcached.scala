af4ddfa8dcd6bc33c6db8804371afcb11991a02f
sxend <arimitsu.k+github@gmail.com>
2016-11-16 01:26:12 +0900

    fix config prefix

--- src/main/scala/arimitsu/sf/platform/www/kvs/Memcached.scala
+++ src/main/scala/arimitsu/sf/platform/www/kvs/Memcached.scala
@@ -1,7 +1,8 @@
 package arimitsu.sf.platform.www.kvs
 
 import akka.actor.ActorSystem
-import com.typesafe.config.ConfigFactory
+import arimitsu.sf.platform.www.PlatformSystem
+import com.typesafe.config.{ Config, ConfigFactory }
 import shade.memcached.{ Configuration, Memcached => Shade }
 
 import scala.concurrent.ExecutionContext
@@ -9,9 +10,10 @@ import scala.concurrent.ExecutionContext
 class Memcached(env: {
   val system: ActorSystem
   val blockingContext: ExecutionContext
+  val config: Config
 }) {
 
-  private val config = env.system.settings.config.getConfig("arimitsu.sf.platform.www.kvs.memcached")
+  private val config = env.config.getConfig(PlatformSystem.withPrefix("kvs.memcached"))
   private val host = config.getString("host")
   private val port = config.getInt("port")
   val client = Shade(Configuration(s"$host:$port"))(env.blockingContext)
