3ef1505414ac42e5a5955d5f6944dd7710bb1dff
sxend <arimitsu.k+github@gmail.com>
2016-12-02 18:16:03 +0900

    add ev var

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CONFIG_RESOURCE=${CONFIG_RESOURCE:-www/application.conf}
 MAIN_CLASS=${MAIN_CLASS:-net.onplatforms.platform.www.WwwSystem}
 
-java -cp /opt/zero.jar -Dconfig.resource=www/application.conf ${MAIN_CLASS}
+java -cp /opt/zero.jar -Dconfig.resource=${CONFIG_RESOURCE} ${MAIN_CLASS}
