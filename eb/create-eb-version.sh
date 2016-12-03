c7e9248035d89965f4857687e64b9ab834bcb3b0
sxend <arimitsu.k+github@gmail.com>
2016-12-03 12:09:08 +0900

    fix version

--- eb/create-eb-version.sh
+++ eb/create-eb-version.sh
@@ -8,6 +8,7 @@ S3KEY=zero/${APP_VERSION}/eb/${ARCHIVE_FILE_NAME}
 # Create ElasticBeanstalk version file by zip command
 cd eb/
 sed -i "s/<APP_VERSION>/${APP_VERSION}/g" Dockerrun.aws.json
+sed -i "s/<APP_VERSION>/${APP_VERSION}/g" .ebextensions/100_load_image.config
 zip ${ARCHIVE_FILE_NAME} --recurse-paths .ebextensions Dockerrun.aws.json
 aws s3 cp ${ARCHIVE_FILE_NAME} s3://internal-storage.onplatforms.net/${S3KEY}
 
