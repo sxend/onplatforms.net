9b39841b419a40cc5d8ffdbd22a594d1fd8f8915
sxend <arimitsu.k+github@gmail.com>
2016-12-04 20:49:35 +0900

    fix typo

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP_PROFILE=${APP_PROFILE:-www}
-CONFIG_RESOURCE=${CONFIG_RESOURCE:-${PROFILE}/application.conf}
-MAIN_CLASS=${MAIN_CLASS:-net.onplatforms.platform.${PROFILE}.Main}
+CONFIG_RESOURCE=${CONFIG_RESOURCE:-${APP_PROFILE}/application.conf}
+MAIN_CLASS=${MAIN_CLASS:-net.onplatforms.platform.${APP_PROFILE}.Main}
 
 java -cp /opt/zero.jar -Dconfig.resource=${CONFIG_RESOURCE} ${MAIN_CLASS}
