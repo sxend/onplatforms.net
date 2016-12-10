7dbc2859ca6a0ca27e04e2422eea41e0fe7ff41c
sxend <arimitsu.k+github@gmail.com>
2016-12-11 01:38:44 +0900

    create dir

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -4,4 +4,6 @@ APP_PROFILE=${APP_PROFILE:-www}
 CONFIG_RESOURCE=${CONFIG_RESOURCE:-${APP_PROFILE}/application.conf}
 MAIN_CLASS=${MAIN_CLASS:-net.onplatforms.${APP_PROFILE}.Main}
 
+mkdir -p logs
+
 java -cp /opt/zero.jar -XX:+UseG1GC -Xloggc:logs/gc.log -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -Dconfig.resource=${CONFIG_RESOURCE} ${MAIN_CLASS}
