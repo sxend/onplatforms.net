470a46e9f068c8896d660212d017f71ea0753344
sxend <arimitsu.k+github@gmail.com>
2016-12-14 12:21:18 +0900

    fix current pathg

--- scripts/build-profile.sh
+++ scripts/build-profile.sh
@@ -4,4 +4,4 @@ APP_PROFILE=$1
 APP_VERSION=$2
 
 docker build -t docker-registry.onplatforms.net/onplatforms.net/zero_${APP_PROFILE}:${APP_VERSION} \
-  --build-arg APP_PROFILE=${APP_PROFILE} --build-arg APP_VERSION=${APP_VERSION} -f ../docker/Dockerfile ../
+  --build-arg APP_PROFILE=${APP_PROFILE} --build-arg APP_VERSION=${APP_VERSION} -f ./docker/Dockerfile ./
--- scripts/release-profile.sh
+++ scripts/release-profile.sh
@@ -4,6 +4,6 @@ APP_PROFILE=$1
 APP_VERSION=$2
 
 docker push docker-registry.onplatforms.net/onplatforms.net/zero_${APP_PROFILE}:${APP_VERSION}
-aws s3 sync ../static/dist/${APP_PROFILE}/ s3://cdn.onplatforms.net/${APP_PROFILE}/${APP_VERSION}/static/
-aws s3 sync ../static/dist/${APP_PROFILE}/ s3://cdn.onplatforms.net/${APP_PROFILE}/latest/static/
-sed -e "s/<APP_PROFILE>/${APP_PROFILE}/" -e "s/<APP_VERSION>/${APP_VERSION}/" ../README.md
+aws s3 sync ./static/dist/${APP_PROFILE}/ s3://cdn.onplatforms.net/${APP_PROFILE}/${APP_VERSION}/static/
+aws s3 sync ./static/dist/${APP_PROFILE}/ s3://cdn.onplatforms.net/${APP_PROFILE}/latest/static/
+sed -e "s/<APP_PROFILE>/${APP_PROFILE}/" -e "s/<APP_VERSION>/${APP_VERSION}/" ./README.md
