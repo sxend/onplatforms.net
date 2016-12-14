b62b891a5dc9d24806f0bf3a160756e2f32b56da
sxend <arimitsu.k+github@gmail.com>
2016-12-15 02:00:34 +0900

    add accounts package

---
+++ src/main/scala/net/onplatforms/accounts/Main.scala
@@ -0,0 +1,43 @@
+package net.onplatforms.accounts
+
+import akka.actor.ActorSystem
+import akka.event.Logging
+import akka.event.Logging._
+import akka.http.scaladsl._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import com.typesafe.config.{Config, ConfigFactory}
+import net.onplatforms.lib.directive.TemplateDirective
+
+import scala.concurrent.ExecutionContext
+
+object Main {
+  val config: Config = ConfigFactory.load
+  val namespace = "net.onplatforms.accounts"
+  def withNamespace(suffix: String) = s"$namespace.$suffix"
+  def getConfigInNamespace(suffix: String): Config = config.getConfig(withNamespace(suffix))
+  val systemConfig: Config = getConfigInNamespace("system")
+  def main(args: Array[String]): Unit = {
+    val env = new {
+      val config: Config = Main.config
+      val namespace: String = Main.namespace
+      val version: String = systemConfig.getString("version")
+      implicit val system = ActorSystem("accounts-system", this.config)
+      val logger = Logging(system.eventStream, getClass)
+      implicit val materializer = ActorMaterializer()
+      val blockingContext: ExecutionContext =
+        system.dispatchers.lookup(withNamespace("dispatchers.blocking-io-dispatcher"))
+      val templateDirectiveImplicits = TemplateDirective.Implicits(this)
+      val indexRouter = new router.IndexRouter(this)
+    }
+    import env._
+
+    val mapping = Seq(
+      get(path("")(indexRouter.handle))
+    ).foldLeft[Route](reject)(_ ~ _)
+
+    val route = logRequest("access", InfoLevel)(mapping)
+    Http().bindAndHandle(route, systemConfig.getString("listen-address"), systemConfig.getInt("listen-port"))
+  }
+}
---
+++ src/main/scala/net/onplatforms/accounts/router/IndexRouter.scala
@@ -0,0 +1,26 @@
+package net.onplatforms.accounts.router
+
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import net.onplatforms.lib.directive.Directives._
+import net.onplatforms.lib.directive.TemplateDirective
+import net.onplatforms.lib.directive.TemplateDirective.Implicits
+
+import scala.util.Success
+
+class IndexRouter(env: {
+                    val templateDirectiveImplicits: TemplateDirective.Implicits
+                    val version: String
+                  }) {
+
+  implicit val templateImplicits: Implicits = env.templateDirectiveImplicits
+
+  def handle: Route = template("accounts/templates/index.html", Map("version" -> env.version)) {
+    case Success(html) => complete(htmlEntity(html))
+    case _             => reject
+  }
+
+  private def htmlEntity(html: String) =
+    HttpEntity(ContentTypes.`text/html(UTF-8)`, html)
+}
